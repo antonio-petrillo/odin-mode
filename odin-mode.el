15f2893b12ab7e05994ba3c225a31fadaedc63d6
mgmarlow <graham@mgmarlow.com>
2023-07-28 21:32:06 -0700

    Language basics

--- odin-mode.el
+++ odin-mode.el
@@ -25,6 +25,120 @@
 
 ;;; Code:
 
+(defgroup odin-mode nil
+  "Major mode for the Odin programming language."
+  :link '(url-link "https://odin-lang.org")
+  :group 'languages)
+
+(defconst odin-keywords
+  '("import" "foreign" "package"
+    "where" "when" "if" "else" "for" "switch" "in" "do" "case"
+    "break" "continue" "fallthrough" "defer" "return" "proc"
+    "struct" "union" "enum" "bit_field" "bit_set" "map" "dynamic"
+    "auto_cast" "cast" "transmute" "distinct" "opaque"
+    "using" "inline" "no_inline"
+    "size_of" "align_of" "offset_of" "type_of"
+    "context"))
+
+(defconst odin-builtins
+  '("len" "cap"
+    "typeid_of" "type_info_of"
+    "swizzle" "complex" "real" "imag" "quaternion" "conj"
+    "jmag" "kmag"
+    "min" "max" "abs" "clamp"
+    "expand_to_tuple"
+
+    "init_global_temporary_allocator"
+    "copy" "pop" "unordered_remove" "ordered_remove" "clear" "reserve"
+    "resize" "new" "new_clone" "free" "free_all" "delete" "make"
+    "clear_map" "reserve_map" "delete_key" "append_elem" "append_elems"
+    "append" "append_string" "clear_dynamic_array" "reserve_dynamic_array"
+    "resize_dynamic_array" "incl_elem" "incl_elems" "incl_bit_set"
+    "excl_elem" "excl_elems" "excl_bit_set" "incl" "excl" "card"
+    "assert" "panic" "unimplemented" "unreachable"))
+
+(defconst odin-constants
+  '("nil" "true" "false"))
+
+(defconst odin-typenames
+  '("bool" "b8" "b16" "b32" "b64"
+
+    "int"  "i8" "i16" "i32" "i64"
+    "i16le" "i32le" "i64le"
+    "i16be" "i32be" "i64be"
+    "i128" "u128"
+    "i128le" "u128le"
+    "i128be" "u128be"
+
+    "uint" "u8" "u16" "u32" "u64"
+    "u16le" "u32le" "u64le"
+    "u16be" "u32be" "u64be"
+
+    "f32" "f64"
+    "complex64" "complex128"
+
+    "quaternion128" "quaternion256"
+
+    "rune"
+    "string" "cstring"
+
+    "uintptr" "rawptr"
+    "typeid" "any"
+    "byte"))
+
+(defvar odin-mode-syntax-table
+  (let ((table (make-syntax-table)))
+    ;; Operators
+    (dolist (i '(?: ?+ ?- ?* ?= ?< ?> ?& ?| ?^ ?! ??))
+      (modify-syntax-entry i "." table))
+
+    ;; Strings
+    (modify-syntax-entry ?\" "\"" table)
+    (modify-syntax-entry ?\\ "\\" table)
+
+    ;; Comments
+    (modify-syntax-entry ?/ ". 124b" table)
+    (modify-syntax-entry ?* ". 23n" table)
+    (modify-syntax-entry ?\n "> b" table)
+    (modify-syntax-entry ?\^m "> b" table)
+
+    table))
+
+(defvar odin-font-lock-keywords
+  `(
+    ;; Keywords
+    (,(regexp-opt odin-keywords 'symbols) . font-lock-keyword-face)
+
+    ;; Types
+    (,(regexp-opt odin-typenames 'symbols) . font-lock-type-face)
+
+    ;; Builtins
+    (,(regexp-opt odin-builtins 'symbols) . font-lock-builtin-face)
+
+    ;; Constants
+    (,(regexp-opt odin-constants 'symbols) . font-lock-constant-face)))
+
+;;;###autoload
+(define-derived-mode odin-mode
+  prog-mode "Odin"
+  "Major mode for the Odin programming language."
+  :group 'odin-mode
+  :syntax-table odin-mode-syntax-table
+
+  (setq-local font-lock-defaults
+              '(odin-font-lock-keywords))
+
+  (setq-local parse-sexp-ignore-comments t)
+  (setq-local comment-start-skip "\\(//+\\|/\\*+\\)\\s *")
+  (setq-local comment-start "/*")
+  (setq-local comment-end "*/")
+
+  ;; Auto indent on }
+  (setq-local electric-indent-chars
+              (append "{}():;," electric-indent-chars)))
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.odin\\'" . odin-mode))
 
 (provide 'odin-mode)
 
